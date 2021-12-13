3fdab9e66a7c295e5d1154a27e5ddc7297c419ba
Michal-Piotrkowski <michal.piotrkowski@protonmail.com>
2021-12-13 11:33:57 +0100

    Wybory dialog

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using System;
+using UnityEngine.EventSystems;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    [Header("Dialogue UI")]
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+
+    [Header("Choices UI")]
+    public  GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
+
+    private Story currentStory;
+
+    public bool dialogueIsPlaying { get; private set; }
+
+    private static DialogueManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.Log("Found more than one Dialogue Manager is the scene");
+        }
+        instance = this;
+    }
+
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Start()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+
+        //get all of the  choices text
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach(GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+    }
+
+    private void Update()
+    {
+        if (!dialogueIsPlaying)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ContinueStory();
+        }
+    }
+
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+
+    }
+
+    private IEnumerator ExitDialogueMode()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "Ej masz problem?";
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+            DisplayChoices();
+        }
+        else
+        {
+            StartCoroutine(ExitDialogueMode());
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        if(currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than the UI can support. Number of choices given: " 
+                + currentChoices.Count);
+        }
+
+        int index = 0;
+        foreach(Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+
+        for(int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+    }
+}
---
+++ Assets/Stories/Dialogue.ink
@@ -0,0 +1,15 @@
+-> main
+
+=== main ===
+
+Za kim jesteś?
+    + [Wisła]
+        -> chosen("Wisł")
+    + [Cracovia]
+        -> chosen("Cracovi")
+    + [Not]
+        -> chosen("Garbarni")
+        
+=== chosen(club) ===
+O ty gnoju wybrałeś {club + "ę"}!
+-> END
